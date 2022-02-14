e5c996ae2b96bff705752976aef40b60553aea8b
bgr906 <bgr906@myy.haaga-helia.fi>
2022-02-14 11:19:35 +0200

    uusi päivitys

--- src/main/java/palvelinohjelmointi/bookstore/web/BookController.java
+++ src/main/java/palvelinohjelmointi/bookstore/web/BookController.java
@@ -3,20 +3,25 @@ package palvelinohjelmointi.bookstore.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import palvelinohjelmointi.bookstore.domain.Book;
+import palvelinohjelmointi.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
 	
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
-	public String getBooks(Model model) {
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String bookList(Model model) {
 		
-		List<Book> books = new ArrayList<Book>();
+		model.addAttribute("books", bookRepository.findAll());
 		
 		return "booklist";
 	}
