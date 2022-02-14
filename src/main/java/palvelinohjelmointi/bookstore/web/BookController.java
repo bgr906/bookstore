a619c8f1afc6564588cfb1568084701be3f552a6
bgr906 <bgr906@myy.haaga-helia.fi>
2022-02-14 11:48:12 +0200

    lisäys ja poisto

--- src/main/java/palvelinohjelmointi/bookstore/web/BookController.java
+++ src/main/java/palvelinohjelmointi/bookstore/web/BookController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,9 +21,25 @@ public class BookController {
 	
 	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
 	public String bookList(Model model) {
-		
 		model.addAttribute("books", bookRepository.findAll());
-		
 		return "booklist";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book) {
+		bookRepository.save(book);
+		return "redirect:/booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String delete(@PathVariable("id") Long id) {
+		bookRepository.deleteById(id);
+		return "redirect:/booklist";
 	}
 }
